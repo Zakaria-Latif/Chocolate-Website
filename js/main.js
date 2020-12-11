3291b40a37700ae06c744ddc4f646d1403b3cf02
Zakaria Latif <73080954+Zakaria-Latif@users.noreply.github.com>
2020-12-11 16:04:03 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready (function() {
+    
+    new WOW().init();
+    
+    
+    $(".burger-nav").on("click", function(){
+        $(".container nav ul").toggleClass("open");
+    })
+    
+    
+   $('.js-wp-2').waypoint(function(direction){
+      
+       $('.js-wp-2').addClass('animated slideInUp');
+   },{
+       offset:'70%'
+   });
+    
+    
+    $('.js-wp-3').waypoint(function(direction){
+      
+       $('.js-wp-3').addClass('animated fadeIn');
+   },{
+       offset:'70%'
+   });
+    
+    
+    $(".iphone-btn").delay(1400).animate({bottom:"+=-3"},300);
+    $(".iphone-btn").delay(300).animate({top:"+=-3"},300);
+
+
+});
